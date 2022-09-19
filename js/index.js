58a690a01ccdadf8699121475f7b36ed07db42fe
ferhad096 <ferhad096@gmail.com>
2022-09-19 13:44:14 +0400

    err

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+const hamburger = document.querySelector(".bar");
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileOverlay = document.querySelector(".header-overlay");
+const closeMenu = document.querySelector(".close-menu");
+
+
+hamburger.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
+
+mobileOverlay.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
+
+closeMenu.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
+
+
 const searchBut = document.querySelector('.search');
 const searchForm = document.querySelector('.search-form');
 const nav = document.querySelector('nav');
