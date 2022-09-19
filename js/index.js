fee7aea54d7510bdde44a6c25d2af8fd9c9e507a
ferhad096 <ferhad096@gmail.com>
2022-09-19 18:12:44 +0400

    final

--- js/index.js
+++ js/index.js
@@ -17,6 +17,25 @@ closeMenu.addEventListener("click", () => {
 });
 
 
+const loginBut = document.querySelector(".login-mobile");
+const mobileLogin = document.querySelector(".mobile-login");
+const loginOverlay = document.querySelector(".login-overlay");
+const closeLogin = document.querySelector(".close-login");
+
+
+loginBut.addEventListener("click", () => {
+  mobileLogin.classList.toggle("active");
+});
+
+loginOverlay.addEventListener("click", () => {
+  mobileLogin.classList.toggle("active");
+});
+
+closeLogin.addEventListener("click", () => {
+  mobileLogin.classList.toggle("active");
+});
+
+
 const searchBut = document.querySelector('.search');
 const searchForm = document.querySelector('.search-form');
 const nav = document.querySelector('nav');
